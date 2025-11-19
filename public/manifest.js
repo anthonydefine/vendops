3b20f5c8a3a9642006f765842224cc8b7e92958f
Anthony Define <anthonydefine@Macs-MacBook-Pro.local>
2025-11-19 18:03:34 -0500

    changed manifest

--- public/manifest.js
+++ public/manifest.js
@@ -1,23 +1,33 @@
 export default function manifest() {
   return {
-    name: 'Next.js PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Next.js',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    name: "Northeast Vending",
+    short_name: "NE Vending",
+    description: "Driver & Admin tools for vending operations.",
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    prefer_related_applications: false,
     icons: [
       {
-        src: '/icon-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/icon-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512-maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
-  }
+    gcm_sender_id: "103953800507",
+    categories: ["business", "productivity"],
+  };
 }
